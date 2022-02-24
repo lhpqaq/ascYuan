050f3272cc18533be94f8fe25f3ee8f63da6d421
lhpqaq <657407891@qq.com>
2022-02-24 14:17:03 +0000

    nice

--- ds_test.py
+++ ds_test.py
@@ -6,6 +6,7 @@ import torch
 import torch.nn as nn
 import deepspeed
 from torch.nn.parallel.distributed import DistributedDataParallel as DDP
+from torch.utils.data.distributed import DistributedSampler
 from deepspeed.runtime.lr_schedules import WarmupLR
 from model.transformer2 import Transformer
 #from model.Transformer_model import Transformer
@@ -16,34 +17,23 @@ from megatron.fp16 import FP16_Module
 import numpy as np
 from torch.utils.data import DataLoader
 from DataSet import TensorDataset
-crossentropyloss=nn.CrossEntropyLoss(reduction='sum')
+import time
 
-class Yuan_loss(nn.Module):
-    def __init__(self):
+class My_CEloss(torch.nn.Module):
+    def __init__(self,reduction='mean'):
         super().__init__()
-        self.cel=nn.CrossEntropyLoss(reduction='sum')
+        self.ce=torch.nn.CrossEntropyLoss(reduction='sum')
+        self.reduction = reduction
     def forward(self, x, y):
-        batch_size = x.size(0)
         loss = 0
         for logit,target in zip(x,y):
-            loss += self.cel(logit,target)
+            loss += self.ce(logit,target)
+        if(self.reduction=='mean'):
+            loss /= x.size(0)
         return loss
+myloss = My_CEloss()
 
-class My_loss(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.nll=torch.nn.NLLLoss(reduction='mean')
-    def forward(self, x, y):
-        batch_size = x.size(0)
-        x = torch.nn.functional.softmax(x,dim=1)
-        x = torch.log(x)
-        loss = 0
-        for logit,target in zip(x,y):
-            loss += self.nll(logit,target)
-        return loss
-myloss = My_loss()
-
-yuan_loss = Yuan_loss()
+#yuan_loss = Yuan_loss()
 
 def create_moe_param_groups(model):
     from deepspeed.moe.utils import is_moe_param
@@ -160,7 +150,7 @@ def main():
     if args.moe:
         deepspeed.utils.groups.initialize(ep_size=args.ep_world_size, mpu=mpu)
 
-    model = Transformer(seq_len=2048,vocab_size=53228,N=40,d_ff=3072,h=24,d_model=480)
+    model = Transformer(seq_len=2048,vocab_size=53228,N=4,d_ff=3072,h=24)
 
     #To prevent OOM for model sizes that cannot fit in GPU memory in full precision
     if args.deepspeed and args.fp16:
@@ -194,40 +184,84 @@ def main():
                                                                     config=args.deepspeed_config)
 
     print('Checkpoint loading...')
-    model_engine.load_checkpoint('./checkpoint/')
+    _,dic = model_engine.load_checkpoint('./checkpoint/')
+    #dic = {}
+    if(not ('step' in dic)):
+        dic['step'] = 0
+
     print('Checkpoint loading completed!')
     #data_loader = get_data_loader(path='data/test_index.txt',batch_size=args.batch_size)
     print('Training data loading...')
     torch_data=TensorDataset('./data/iData0.npy',if_dir=False)
+    sampler = DistributedSampler(torch_data)
+    data_loader = torch.utils.data.DataLoader(torch_data,
+                                               pin_memory=True,
+                                               batch_size=args.batch_size,
+                                               shuffle=False,
+                                               sampler=sampler)
     #torch_data=TensorDataset('./data/',if_dir=True)
-    datas = DataLoader(torch_data, batch_size=args.batch_size, shuffle=False, drop_last=False)
+    #datas = DataLoader(torch_data, batch_size=args.batch_size, shuffle=False, drop_last=False)
     print('Training data loading completed!')
     #torch.distributed.broadcast(data_loader,
     #                            mpu.get_model_parallel_src_rank(),
     #                            group=mpu.get_model_parallel_group())
-    model_engine.save_checkpoint('./checkpoint/')
-    loop = tqdm(enumerate(datas),total=len(datas))
+    #model_engine.save_checkpoint('./checkpoint/')
+    loop = tqdm(enumerate(data_loader),total=len(data_loader))
+    start = dic['step']
+    # print('-----------------------------------------------------\n\n',start,'-----------------------------------------------------\n\n')
+    # for i,Input in loop:
+    #     loop.set_description('Train')
+    #     if args.fp16:
+    #         Input = Input.half()
+    #     Input = Input.to(model_engine.device).long()
+    #     #Input = torch.LongTensor(Input)
+    #     out=model_engine(Input)
+    #     #out = out.view(-1,out.shape[-1])
+    #     #Input = Input.view(-1).long()
+    #     #print('Input:',Input.shape)
+    #     #print('out:',out.shape)
+    #     #loss = crossentropyloss(out,Input)
+    #     loss = myloss(out,Input)
+    #     log = 'loss:' + str(loss.item()) + '\t\ttokens_num:' + str((i+1)*args.batch_size*2048/100000) + 'HT'
+    #     logging.info(log)
+    #     loop.set_postfix(loss=loss.item()/4096)
+    #     #print('loss:\n',loss.item()*2048)
+    #     model_engine.backward(loss)
+    #     model_engine.step()
+    #     if(i%100==0):
+    #         dic['step'] = i
+    #         model_engine.save_checkpoint('./checkpoint/',client_state=dic)
+
+
+    crossentropyloss = My_CEloss('sum')
+    since = time.time()
+    print()
     for i,Input in loop:
-        loop.set_description('Train')
+        if(i<=start):
+            continue
+        rank = torch.distributed.get_rank()
+        loop.set_description('Rank '+str(rank))
         if args.fp16:
             Input = Input.half()
         Input = Input.to(model_engine.device).long()
-        #Input = torch.LongTensor(Input)
         out=model_engine(Input)
         #out = out.view(-1,out.shape[-1])
         #Input = Input.view(-1).long()
         #print('Input:',Input.shape)
         #print('out:',out.shape)
-        #loss = crossentropyloss(out,Input)
-        loss = myloss(out,Input)
-        log = 'loss:' + str(loss.item()/args.batch_size) + '\t\ttokens_num:' + str((i+1)*args.batch_size*2048/100000) + 'HT'
+        loss=crossentropyloss(out,Input)
+        log = 'rank'+str(rank)+'\tloss:' + str(loss.item()) + '\t\ttokens_num:' + str((i+1)*args.batch_size*2048/100000) + 'HT'
         logging.info(log)
-        loop.set_postfix(loss=loss.item()/args.batch_size)
-        #print('loss:\n',loss.item()*2048)
+        loop.set_postfix(loss=loss.item())
         model_engine.backward(loss)
         model_engine.step()
+        lr_scheduler.step()
         if(i%1000==0):
-            model_engine.save_checkpoint('./checkpoint/')
+            dic['step'] = i
+            model_engine.save_checkpoint('./checkpoint/',client_state=dic)
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
         #lr_scheduler.step()
 
 if __name__ == "__main__":
